55ebb5ead1792efc579238fa66d234c2293f9b65
mdriaz <riazmd582@gmail.com>
2020-05-03 23:29:26 +0600

    live slug making with jquery

--- resources/views/partials/_scripts.blade.php
+++ resources/views/partials/_scripts.blade.php
@@ -5,6 +5,7 @@
 <script src="{{asset('js/toastr.min.js')}}"></script>
 <script src="{{ asset('js/prism.js') }}"></script>
 <script src="{{ asset('js/app.js') }}"></script>
+<!-- copy url from share method -->
 <script>
     function copyToClipboard(text) {
         var dummy = document.createElement("textarea");
@@ -19,34 +20,41 @@
         document.body.removeChild(dummy);
         alert('copied!');
     }
-$(document).ready(function () {
- // Toastr Notification
-    @if(Session::has('message'))
-    var type = "{{ Session::get('alert-type', 'info') }}";
-    switch (type) {
-    case 'info':
-    toastr.info("{{ Session::get('message') }}");
-    break;
 
-    case 'warning':
-    toastr.warning("{{ Session::get('message') }}");
-    break;
+    // jquery toastr message -->
+    $(document).ready(function () {
+        // Toastr Notification
+            @if(Session::has('message'))
+        var type = "{{ Session::get('alert-type', 'info') }}";
+        switch (type) {
+            case 'info':
+                toastr.info("{{ Session::get('message') }}");
+                break;
 
-    case 'success':
-    toastr.success("{{ Session::get('message') }}");
-    break;
+            case 'warning':
+                toastr.warning("{{ Session::get('message') }}");
+                break;
 
-    case 'error':
-    toastr.error("{{ Session::get('message') }}");
-    break;
+            case 'success':
+                toastr.success("{{ Session::get('message') }}");
+                break;
+
+            case 'error':
+                toastr.error("{{ Session::get('message') }}");
+                break;
+        }
+        @endif
+    });
+    // slug making
+    $('input#title').keyup(function (e) {
+        var slug = $('input#title').val().toLowerCase().replace(/ /g, "-");
+        $('input#slug').val(slug);
+    });
+
+    // Remove 000webhost branding -->
+    window.onload = () => {
+        let bannerNode = document.querySelector('[alt="www.000webhost.com"]').parentNode.parentNode;
+        bannerNode.parentNode.removeChild(bannerNode);
     }
-    @endif
-});
-
-  // Remove 000webhost branding -->
-window.onload = () => {
-   let bannerNode = document.querySelector('[alt="www.000webhost.com"]').parentNode.parentNode;
-   bannerNode.parentNode.removeChild(bannerNode);
-}
 
 </script>
