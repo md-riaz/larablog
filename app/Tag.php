bce29614eb0443ef8150d5014424cdd60e74728b
mdriaz <riazmd582@gmail.com>
2020-05-05 00:25:40 +0600

    setup gmail with env file

--- app/Tag.php
+++ app/Tag.php
@@ -4,9 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Tag extends Model
-{
+class Tag extends Model {
+
     protected $fillable = ['name'];
+
     // A tag belongs to many posts
     public function posts()
     {
