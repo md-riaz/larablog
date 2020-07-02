b4d85946c682c536b5e04f5572aefc6f770248cd
mdriaz <riazmd582@gmail.com>
2020-07-02 21:00:26 +0600

    magic link btn fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,93 +3,90 @@
 
 @section('content')
 
-    <div class="col">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header bg-sienna text-white">{{ __('Login') }}</div>
+<div class="col">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header bg-sienna text-white">{{ __('Login') }}</div>
 
-                    <div class="card-body">
+                <div class="card-body">
 
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
 
-                            <div class="form-group">
-                                <label for="email" class="col-md-12 col-form-label">{{ __('E-Mail Address') }}</label>
+                        <div class="form-group">
+                            <label for="email" class="col-md-12 col-form-label">{{ __('E-Mail Address') }}</label>
 
-                                <div class="col-md-12">
-                                    <input id="email" type="email"
-                                           class="form-control @error('email') is-invalid @enderror"
-                                           name="email" value="{{ old('email') }}" required autocomplete="email"
-                                           autofocus>
+                            <div class="col-md-12">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                                    name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                                    @error('email')
-                                    <span class="invalid-feedback" role="alert">
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
-                                    @enderror
-                                </div>
+                                @enderror
                             </div>
+                        </div>
 
-                            <div class="form-group">
-                                <label for="password" class="col-md-12 col-form-label">{{ __('Password') }}</label>
+                        <div class="form-group">
+                            <label for="password" class="col-md-12 col-form-label">{{ __('Password') }}</label>
 
-                                <div class="col-md-12">
-                                    <input id="password" type="password"
-                                           class="form-control @error('password') is-invalid @enderror" name="password"
-                                           autocomplete="current-password">
+                            <div class="col-md-12">
+                                <input id="password" type="password"
+                                    class="form-control @error('password') is-invalid @enderror" name="password"
+                                    autocomplete="current-password">
 
-                                    @error('password')
-                                    <span class="invalid-feedback" role="alert">
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
-                                    @enderror
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group ">
+                            <div class="col-md-12">
+                                <div class="form-check">
+                                    <input class="form-check-input " type="checkbox" name="remember" id="remember"
+                                        {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
                                 </div>
                             </div>
+                        </div>
+                        @if (session('message'))
+                        <div class="alert alert-success" role="alert">
+                            {{session('message')}}
+                        </div>
+                        @endif
+                        <div class="form-group mb-0">
+                            <div class="col-md-12">
+                                <button type="submit" class="btn bg-sienna text-white px-5">
+                                    Login
+                                </button>
+                                <button type="submit" class="btn bg-dark text-white px-5" name="submit"
+                                    value="magic-link">
+                                    Send Magin Link
+                                </button>
 
-                            <div class="form-group ">
-                                <div class="col-md-12">
-                                    <div class="form-check">
-                                        <input class="form-check-input " type="checkbox" name="remember" id="remember"
-                                            {{ old('remember') ? 'checked' : '' }}>
+                                @if (Route::has('password.request'))
+                                <a class="btn btn-link float-right text-sienna" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
+                                @endif
 
-                                        <label class="form-check-label" for="remember">
-                                            {{ __('Remember Me') }}
-                                        </label>
-                                    </div>
-                                </div>
+
                             </div>
-                            @if (session('message'))
-                                <div class="alert alert-success" role="alert">
-                                    {{session('message')}}
-                                </div>
-                            @endif
-                            <div class="form-group mb-0">
-                                <div class="col-md-12">
-                                    <button type="submit" class="btn bg-sienna text-white px-5">
-                                        Login
-                                    </button>
-                                    <button type="submit" class="btn bg-dark text-white px-5" name="submit"
-                                            value="magic-link">
-                                        Send Magin Link
-                                    </button>
-
-                                    @if (Route::has('password.request'))
-                                        <a class="btn btn-link float-right text-sienna"
-                                           href="{{ route('password.request') }}">
-                                            {{ __('Forgot Your Password?') }}
-                                        </a>
-                                    @endif
-
-
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
 
 
